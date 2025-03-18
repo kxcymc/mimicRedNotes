6ab0ecb7cb6f506f907e7f2ed0a790d31c8c5f40
mono <111648739+kxcymc@users.noreply.github.com>
2025-03-18 08:29:46 +0800

    Delete mimicRedNotes directory

--- mimicRedNotes/src/store/modules/homePage.ts
+++
@@ -1,9 +0,0 @@
-import { defineStore } from "pinia";
-import { ref } from "vue";
-
-const useHomePageStore = defineStore("homePage", () => {
-  const refresh = ref<boolean>(false); // 导航右侧刷新按钮效果 
-  return { refresh };
-});
-
-export default useHomePageStore;
